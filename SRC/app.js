5b1f1cd937fcf1f2f4ed55e58c2d68ea9d0b5a0c
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-06-05 15:34:55 -0500

    changes from the other day I forgot to commit - getting everything nice and changed for ES 6 syntax

--- SRC/app.js
+++ SRC/app.js
@@ -1,13 +1,28 @@
-var logger = require('morgan');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var indexRouter = require('./routes/index');
-var app = express();
-
+// ES6 way
+import logger from 'morgan';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import indexRouter from './routes/index';
+const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
 
-module.exports = app;
+export default app;
+
+// ES5 way below
+// var logger = require('morgan');
+// var express = require('express');
+// var cookieParser = require('cookie-parser');
+// var indexRouter = require('./routes/index');
+// var app = express();
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use('/v1', indexRouter);
+
+// module.exports = app;
--- SRC/routes/index.js
+++ SRC/routes/index.js
@@ -1,6 +1,18 @@
-var express = require('express');
-var router = express.Router();
-router.get('/', function(req, res, next) {
-  return res.status(200).json({ message: 'Welcome to Express API template' });
-});
-module.exports = router;
+// ES6 way
+import express from 'express';
+
+const indexRouter = express.Router();
+
+indexRouter.get('/', (req, res) =>
+  res.status(200).json({ message: 'Welcome to EXPRESS API Template ::)'})
+);
+
+export default indexRouter;
+
+// ES5 way below
+// var express = require('express');
+// var router = express.Router();
+// router.get('/', function(req, res, next) {
+//   return res.status(200).json({ message: 'Welcome to Express API template' });
+// });
+// module.exports = router;
